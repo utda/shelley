355e6c8e8e6a8e11112205d5a79c1a594d2df0cf
nakamura196 <na.kamura.1263@gmail.com>
2020-08-09 11:54:47 +0900

    initial commit

---
+++ src/010_static/convert.sh
@@ -0,0 +1,3 @@
+python iiif_static/iiif_static.py -d ../../docs/iiif -t 200 -p https://utda.github.io/shelly/iiif/toka ../image/toka.jpg 
+python iiif_static/iiif_static.py -d ../../docs/iiif -t 200 -p https://utda.github.io/shelly/iiif ../image/original.jpg
+python iiif_static/iiif_static.py -d ../../docs/iiif2 -t 1000 -p https://utda.github.io/shelly/iiif2 ../image/original.jpg 
