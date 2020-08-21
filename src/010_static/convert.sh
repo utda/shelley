dcc6743492e6d2739416a037041d16fa5fc0f4ea
nakamura196 <na.kamura.1263@gmail.com>
2020-08-21 18:29:29 +0900

    update data

--- src/010_static/convert.sh
+++ src/010_static/convert.sh
@@ -1,3 +1,2 @@
-python iiif_static/iiif_static.py -d ../../docs/iiif -t 200 -p https://utda.github.io/shelly/iiif/toka ../image/toka.jpg 
-python iiif_static/iiif_static.py -d ../../docs/iiif -t 200 -p https://utda.github.io/shelly/iiif ../image/original.jpg
-python iiif_static/iiif_static.py -d ../../docs/iiif2 -t 1000 -p https://utda.github.io/shelly/iiif2 ../image/original.jpg 
+python iiif_static/iiif_static.py -d ../../docs/files/tile -t 200 -p https://utda.github.io/shelly/files/tile ../../docs/files/original/toka.jpg 
+python iiif_static/iiif_static.py -d ../../docs/files/tile -t 200 -p https://utda.github.io/shelly/files/tile ../../docs/files/original/original.jpg
