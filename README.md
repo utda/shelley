efc34cf88d821c78f6f2dadd239a22f6cf70bc79
nakamura196 <na.kamura.1263@gmail.com>
2022-01-05 19:34:15 +0900

    feat: replace str

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# shelly
+# shelley
 シェリー書簡の透過光画像を使ったIIIFアプリケーション
 
-https://utda.github.io/shelly/
+https://utda.github.io/shelley/
--- src/010_static/convert.sh
+++ src/010_static/convert.sh
@@ -1,2 +1,2 @@
-python iiif_static/iiif_static.py -d ../../docs/files/tile -t 200 -p https://utda.github.io/shelly/files/tile ../../docs/files/original/toka.jpg 
-python iiif_static/iiif_static.py -d ../../docs/files/tile -t 200 -p https://utda.github.io/shelly/files/tile ../../docs/files/original/original.jpg
+python iiif_static/iiif_static.py -d ../../docs/files/tile -t 200 -p https://utda.github.io/shelley/files/tile ../../docs/files/original/toka.jpg 
+python iiif_static/iiif_static.py -d ../../docs/files/tile -t 200 -p https://utda.github.io/shelley/files/tile ../../docs/files/original/original.jpg
